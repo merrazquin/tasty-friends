5b205306a51d5c094f6e1fd03ddfc0483108bd83
Marcela Errazquin <merrazquin@gmail.com>
2018-07-09 21:33:45 -0400

    Forcing ssl redirect on heroku

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const sslRedirect = require('heroku-ssl-redirect')
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(sslRedirect())
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+    app.use(express.static('client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
@@ -19,6 +22,6 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tastyfriends');
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
